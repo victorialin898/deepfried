c2d6f079eb2de98d3d7bfd21295b316271037a18
Kevin Ouyang <kevin_ouyang@brown.edu>
2019-12-10 14:23:06 -0500

    update train and test to use dataset iterator

--- main.py
+++ main.py
@@ -9,22 +9,23 @@ import time
 import soundfile as sf
 
 """
+    @param batch : a tensor of (batch_size, 6000)
+"""
+def corrupt_batch(batch):
+    pass
+
+"""
     Trains model in batches on corrupted and original audio files.
 """
-
 def train(model, train_data_iterator):
-    # TODO: reimplement parts of this using the train_data_iterator
-
-    bs = model.batch_size
-    for i in range(len(corrupted) // bs):
-        batch_corrupted = corrupted[i*bs : (i+1)*bs]
-        batch_originals = originals[i*bs : (i+1)*bs]
+    for iteration, batch in enumerate(train_data_iterator):
+        batch_corrupted = corrupt_batch(batch)
 
         with tf.GradientTape() as tape:
             batch_sharpened = model.call(batch_corrupted)
-            loss = model.loss_function(batch_sharpened, batch_originals)
+            loss = model.loss_function(batch_sharpened, batch)
 
-        gradients = tape.gradient(loss, model.variables) # Maybe trainable_variables here?
+        gradients = tape.gradient(loss, model.trainable_variables) # Maybe trainable_variables here?
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 """
@@ -33,23 +34,15 @@ def train(model, train_data_iterator):
     @returns : average loss over input test data, sharpened audio files
 """
 def test(model, test_data_iterator):
-    # TODO: reimplement parts of this using the test_data_iterator
-
     losses = []
-    sharpened = []
 
-    bs = model.batch_size
-    for i in range(len(corrupted) // bs):
-        batch_corrupted = corrupted[i*bs : (i+1)*bs]
-        batch_originals = originals[i*bs : (i+1)*bs]
+    for iteration, batch in enumerate(test_data_iterator):
+        batch_corrupted = corrupt_batch(batch)
+        batch_sharpened = model.call(batch_corrupted)
+        loss = model.loss_function(batch_sharpened, batch_originals)
+        losses.append(loss)
 
-        with tf.GradientTape() as tape:
-            batch_sharpened = model.call(batch_corrupted)
-            sharpened += batch_sharpened
-            loss = model.loss_function(batch_sharpened, batch_originals)
-            losses.append(loss)
-
-    return tf.reduce_mean(losses), sharpened
+    return tf.reduce_mean(losses)
 
 def main():
     if len(sys.argv) != 2 or sys.argv[1] not in {"VCTK","PIANO"}:
@@ -57,19 +50,18 @@ def main():
         print("<Data Set>: [VCTK/PIANO]")
         exit()
 
+    model = Model()
+
     print("Running preprocessing...")
     if sys.argv[1] == "VCTK":
-        train_data_iterator, test_data_iterator = get_dataset_iterator(VCTK=True)
+        train_data_iterator, test_data_iterator = get_dataset_iterator(batch_size=model.batch_size, VCTK=True)
     elif sys.argv[1] == "PIANO":
-        train_data_iterator, test_data_iterator = get_dataset_iterator(VCTK=False)
+        train_data_iterator, test_data_iterator = get_dataset_iterator(batch_size=model.batch_size, VCTK=False)
     print("Finished reprocessing.")
-    
+
     # TODO: remove this loop. this is just to test that our data is the correct shape
     for iteration, data in enumerate(train_data_iterator):
         print(iteration, data.shape)
-
-
-    model = Model()
 
     print("Beginning training...")
     for _ in range(model.epochs):
@@ -77,12 +69,14 @@ def main():
     print("Training complete.")
 
     print("Beginning testing...")
-    loss, sharpened = test(model, test_data_iterator)
+    loss = test(model, test_data_iterator)
     print("Average loss: " + str(loss.numpy()))
 
+    # TODO: figure out a way to write some files to disk as demo
+    # below is an attempt but probably doesn't work
     # Write 20 sharpened files as .wav for demo purposes
-    for i in range(len(demo_sr)):
-        sf.write(file=os.path.join('output', str(i + 1)+'.wav'), data=sharpened, samplerate=demo_sr[i])
+    # for i in range(len(demo_sr)):
+    #     sf.write(file=os.path.join('output', str(i + 1)+'.wav'), data=sharpened, samplerate=demo_sr[i])
 
 
 if __name__ == '__main__':
